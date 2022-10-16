8fd0c9cb8b807797dd0b85d4f3f2de297315eefb
sherief <sheriefsaad10@gmail.com>
2022-10-16 19:44:42 +0200

    add buttons for player selection in game function

--- index.js
+++ index.js
@@ -2,24 +2,72 @@ const choices = ["rock", "paper", "scissors"];
 
 
 function game(){
-    bestOfFive();
+    let buttons = document.querySelectorAll('button');
+    buttons.forEach((button) =>
+        button.addEventListener('click', () => {
+            if(button.id){
+                playRound(button.id);
+            }
+        })
+    );
 }
 
-function playRound(){
+function playRound(playerChoice){
     const computerSelection = computerChoice();
-    const playerSelection = playerChoice();        
-    const winner = checkWinner(playerSelection, computerSelection);
+    //const playerSelection = playerChoice();        
+    const winner = checkWinner(playerChoice, computerSelection);
 
     console.log(winner);
 
-    return {computerSelection, playerSelection, winner};
+    //return {computerSelection, playerSelection, winner};
 }
 
+/*
+// rock button
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', playRound);
+
+// paper button
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', playerChoice);
+
+// scissors button
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', playerChoice);
+*/
+
+/*
 function playerChoice(){
+    console.log("abos edeky et7ally");
+    /*
     let input = prompt("Type Rock, Paper or scissors.");
     input = input.toLowerCase();
     return input;
-}
+    */
+
+    /*
+    let input;
+
+
+    const rock = document.querySelector('#rock');
+    rock.addEventListener('click', () => {
+        input = "rock";
+    });
+
+    const paper = document.querySelector('#paper');
+    rock.addEventListener('click', () => {
+        input = "paper";
+    });
+
+    const scissors = document.querySelector('#scissors');
+    rock.addEventListener('click', () => {
+        input = "scissors   ";
+    });
+
+    return input;
+    */
+//}
+
 
 function computerChoice(){
     return choices[Math.floor(Math.random() * choices.length)];
